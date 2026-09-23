427ec7f8192a101ae18c2dace9c07d7204505062
2766398 <2766398@learn.vsb.bc.ca>
2026-09-23 14:55:35 -0700

    Update Multiadd.java

--- Multiadd.java
+++ Multiadd.java
@@ -3,9 +3,14 @@ public class Multiadd{
 		System.out.println(multadd(1.0, 2.0, 3.0));
 		System.out.println(multadd(1.0, Math.cos(Math.PI/4), Math.sin(Math.PI/4)));
 		System.out.println(multadd(0.5, Math.log(10), Math.log(20)));
+		System.out.println(expSum(5.0));
 	}
 	public static double multadd(double a, double b, double c){
 		double result = a * b + c;
+		return result;
+	}
+	public static double expSum(double d){
+		double result = multadd(d, Math.exp(-d), Math.sqrt(1-Math.exp(-d)));
 		return result;
 	}
 }
